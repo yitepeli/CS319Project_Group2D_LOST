c99db79d48814a8c2076dc33b72677725bb12cb6
yitepeli <yitepeli@gmail.com>
2017-04-30 02:54:56 +0300

    mainmenu

---
+++ LOSTPROJECT/src/GUI/MainMenu.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainMenu {
+
+	public MainMenu() {
+		JFrame frame1 = new JFrame();
+		frame1.setSize(500, 500);
+		frame1.setVisible(true);
+		JPanel myPanel = new JPanel();
+	}
+	
+	public static void main(String[] args) {
+		MainMenu benimki = new MainMenu();
+
+	}
+
+}
