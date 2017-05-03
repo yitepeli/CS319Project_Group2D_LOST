b4658cad7d2fb9ad4309283ceaf2e2d8684eee5c
sonmezonur <onursonmez6116@gmail.com>
2017-05-03 14:22:47 +0300

    Constant holder

---
+++ LOSTPROJECT/src/GameObjectsManagement/ObjectManagement/Constants.java
@@ -0,0 +1,29 @@
+package GameObjectsManagement.ObjectManagement;
+
+/**
+ * Author: Onur Sönmez
+ * Description: Holder for constant variables in game, instead of including them in classes and providing poor syntax,
+ *              this applies more readable and protectable hierarchy. Most of constants will be used in database querying
+ *              and do not want to involve them into object-oriented design.
+ */
+
+
+public class Constants {
+
+    //For database querying...
+    public final static String OBJECT_NAME = "objectName";
+    public final static String OBJECT_ID = "objectId";
+    public final static String DESCRIPTION ="description";
+    public final static String ACCOMPLISHED_STORY_EVENT = "accomplishedStoryEvent";
+    public final static String ACCOMPLISHED_GAME_TIME = "accomplishedGameTime";
+    public final static String IS_ACCOMPLISHED = "isAccomplished";
+    public final static String REAL_TIME = "realTime";
+    public final static String HEALTH = "health";
+    public final static String DEFENSE = "defense";
+    public final static String ATTACK = "attack";
+    public final static String ENERGY = "energy";
+    public final static String HUNGER = "hunger";
+    public final static String THIRST = "thirst";
+    public final static String CURRENT_AREA = "currentArea";
+
+}
